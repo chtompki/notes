bbcf9445763911d6155253f27ead62cdffce4389
Rob Tompkins <chtompki@gmail.com>
2017-12-13 08:09:23 -0500

    Create svn_artifact_mover.sh

---
+++ commons-release-scripts/svn_artifact_mover.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/RELEASE-NOTES.txt https://dist.apache.org/repos/dist/release/commons/text/RELEASE-NOTES.txt -m "Releasing commons-text-1.2"
+
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/binaries/commons-text-1.2-bin.tar.gz https://dist.apache.org/repos/dist/release/commons/text/binaries/commons-text-1.2-bin.tar.gz -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/binaries/commons-text-1.2-bin.tar.gz.asc https://dist.apache.org/repos/dist/release/commons/text/binaries/commons-text-1.2-bin.tar.gz.asc -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/binaries/commons-text-1.2-bin.tar.gz.md5 https://dist.apache.org/repos/dist/release/commons/text/binaries/commons-text-1.2-bin.tar.gz.md5 -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/binaries/commons-text-1.2-bin.tar.gz.sha1 https://dist.apache.org/repos/dist/release/commons/text/binaries/commons-text-1.2-bin.tar.gz.sha1 -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/binaries/commons-text-1.2-bin.zip https://dist.apache.org/repos/dist/release/commons/text/binaries/commons-text-1.2-bin.zip -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/binaries/commons-text-1.2-bin.zip.asc https://dist.apache.org/repos/dist/release/commons/text/binaries/commons-text-1.2-bin.zip.asc -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/binaries/commons-text-1.2-bin.zip.md5 https://dist.apache.org/repos/dist/release/commons/text/binaries/commons-text-1.2-bin.zip.md5 -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/binaries/commons-text-1.2-bin.zip.sha1 https://dist.apache.org/repos/dist/release/commons/text/binaries/commons-text-1.2-bin.zip.sha1 -m "Releasing commons-text-1.2"
+
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/source/commons-text-1.2-src.tar.gz https://dist.apache.org/repos/dist/release/commons/text/source/commons-text-1.2-src.tar.gz -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/source/commons-text-1.2-src.tar.gz.asc https://dist.apache.org/repos/dist/release/commons/text/source/commons-text-1.2-src.tar.gz.asc -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/source/commons-text-1.2-src.tar.gz.md5 https://dist.apache.org/repos/dist/release/commons/text/source/commons-text-1.2-src.tar.gz.md5 -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/source/commons-text-1.2-src.tar.gz.sha1 https://dist.apache.org/repos/dist/release/commons/text/source/commons-text-1.2-src.tar.gz.sha1 -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/source/commons-text-1.2-src.zip https://dist.apache.org/repos/dist/release/commons/text/source/commons-text-1.2-src.zip -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/source/commons-text-1.2-src.zip.asc https://dist.apache.org/repos/dist/release/commons/text/source/commons-text-1.2-src.zip.asc -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/source/commons-text-1.2-src.zip.md5 https://dist.apache.org/repos/dist/release/commons/text/source/commons-text-1.2-src.zip.md5 -m "Releasing commons-text-1.2"
+svn mv https://dist.apache.org/repos/dist/dev/commons/text/source/commons-text-1.2-src.zip.sha1 https://dist.apache.org/repos/dist/release/commons/text/source/commons-text-1.2-src.zip.sha1 -m "Releasing commons-text-1.2"
